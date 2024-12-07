c65dc1524005e79ec02258134591e531b774bbdb
Carita Lindfors <caritalindf@gmail.com>
2024-12-07 19:12:37 +0200

    RetrofitInstance.kt

---
+++ harjoitusprojekti/app/src/main/java/com/example/harjoitusprojekti/RetrofitInstance.kt
@@ -0,0 +1,24 @@
+package com.example.harjoitusprojekti
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
+object RetrofitInstance {
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://fakestoreapi.com/")
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val api: ApiService = retrofit.create(ApiService::class.java)
+}
