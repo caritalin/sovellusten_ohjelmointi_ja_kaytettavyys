51a6885b8194ad65bbd73c359f5d7700eb653d6f
Carita Lindfors <caritalindf@gmail.com>
2024-12-07 19:12:57 +0200

    models/: add

---
+++ harjoitusprojekti/app/src/main/java/com/example/harjoitusprojekti/models/Product.kt
@@ -0,0 +1,8 @@
+package com.example.harjoitusprojekti.models
+
+data class Product(
+    val id: Int,
+    val title: String,
+    val price: Double,
+    val image: String
+)
---
+++ harjoitusprojekti/app/src/main/java/com/example/harjoitusprojekti/models/ShoppingItem.kt
@@ -0,0 +1,8 @@
+package com.example.harjoitusprojekti.models
+
+data class ShoppingItem(
+    val id: Int,
+    val title: String,
+    val price: Double,
+    val image: String
+)
