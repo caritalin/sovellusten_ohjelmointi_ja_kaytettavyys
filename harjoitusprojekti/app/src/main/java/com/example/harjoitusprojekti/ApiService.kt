bbe808512e34cbc9fe8709906090f735e9f136f6
Carita Lindfors <caritalindf@gmail.com>
2024-12-07 19:13:14 +0200

    ApiService.kt

---
+++ harjoitusprojekti/app/src/main/java/com/example/harjoitusprojekti/ApiService.kt
@@ -0,0 +1,9 @@
+package com.example.harjoitusprojekti
+
+import com.example.harjoitusprojekti.models.Product
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("products")
+    suspend fun getProducts(): List<Product>
+}
